48a2de2614fb1c7f7ef00a51e3f45378ba97f7f3
liushijie <liushijie@gmail.com>
2019-06-02 16:37:01 +0800

    不完整selenium

--- Python爬虫/第七章/012.py
+++ Python爬虫/第七章/012.py
@@ -3,15 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ES
 from selenium.webdriver.support.wait import WebDriverWait
 import time
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException,StaleElementReferenceException
+from pyquery import PyQuery  as pq
+import pymongo
+
 
 browser = webdriver.Chrome()
-wait = WebDriverWait(browser, 5)
+wait = WebDriverWait(browser, 10)
 
+
+client = pymongo.MongoClient(host='47.106.109.80', port=27017)
+db = client['jingdong']
+db.products.drop()
+collection = db['products']
 
 def search():
     browser.get('https://www.jd.com/')
     try:
+        time.sleep(2)
         input = wait.until(ES.presence_of_element_located(
             (By.CSS_SELECTOR, '#key')))
         submit = wait.until(ES.element_to_be_clickable(
@@ -28,19 +37,62 @@ def next_page(page):
             (By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > input')))
         next_submit = wait.until(ES.element_to_be_clickable(
             (By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > a')))
-        next_input.clear()
-        next_input.send_keys(page)
-        next_submit.click()
+        try:
+            next_input.clear()
+            next_input.send_keys(page)
+            next_submit.click()
+        except StaleElementReferenceException:
+            next_input = wait.until(ES.presence_of_element_located(
+            (By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > input')))
+            next_submit = wait.until(ES.element_to_be_clickable(
+            (By.CSS_SELECTOR, '#J_bottomPage > span.p-skip > a')))
+            next_input.clear()
+            next_input.send_keys(page)
+            next_submit.click()
+
+
         wait.until(ES.text_to_be_present_in_element(
             (By.CSS_SELECTOR, '#J_bottomPage > span.p-num > a.curr'), str(page)))
-        time.sleep(1)
-        # time.sleep(3)
-        # return browser.page_source
+        wait.until(ES.presence_of_element_located((By.CSS_SELECTOR, '#J_goodsList > ul > li')))
+        get_procudts()
     except TimeoutException:
         next_page(page)
+
+def get_procudts():
+    time.sleep(3)
+    html = browser.page_source
+    doc = pq(html)
+    items_list = doc('#J_goodsList > ul > li').items()
+    k=0
+    for item in items_list:
+        k+=1
+        products = {
+            'price': item.find('div > div.p-price > strong > i').text(),
+            'detail': item.find('div > div.p-name.p-name-type-2 > a > em').text(),
+            'shop': item.find('div > div.p-shop > span > a').text()
+        }
+        print(products)
+        save_mongodb(products)
+    print('找到的总数是：{}'.format(k))
+
+def save_mongodb(products):
+    try:
+        collection.insert_one(products)
+        print('保存成功')
+    except Exception:
+        print('保存失败')
+
+
 
 
 if __name__ == "__main__":
     search()
-    for i in range(1, 20):
+    for i in range(1, 10):
+        print('*'*100)
+        print('page'+str(i))
+        print('*'*100)
         next_page(i)
+        
+    browser.close()
+
+
